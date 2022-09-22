59c1b0ca8f0eef190beb8ffbfb2f8acc3909ce5f
Steven Collins <StevenCollins@users.noreply.github.com>
2022-09-21 23:23:55 -0400

    Updated idb version
    Updated idb version to support getAllFromIndex,
    added a colourless index,
    added test code for pulling data from the db and making it unique

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ document.getElementById("latestVersion").innerText = ("Latest Version: " + lates
 // Open and initialize DB
 initializeDB(); // We want to do this on page load
 async function initializeDB() {
-    db = await idb.openDb(dbName, latestDBVersion, db => {
-        db.createObjectStore("meta");
-        const objectStore = db.createObjectStore("commander", { keyPath: "name" });
-        objectStore.createIndex("colorIdentityIndex", "colorIdentity", { multiEntry: true });
-    });
-    checkVersion();
+    try {
+        db = await idb.openDB(dbName, latestDBVersion, {
+            upgrade(db) {
+                db.createObjectStore("meta");
+                const objectStore = db.createObjectStore("commander", { keyPath: "name" });
+                objectStore.createIndex("colorIdentityIndex", "colorIdentity", { multiEntry: true });
+                objectStore.createIndex("colorlessIdentityIndex", "colorIdentity", { multiEntry: false }); // does this actually get colorless?
+            }
+        });
+        checkVersion();
+    } catch {
+        document.getElementById("currentVersion").innerText = ("Your browser doesn't support IndexedDB :(");
+        document.getElementById("loadDeck").disabled = true;
+        document.getElementById("createDeck").disabled = true;
+    }
 }
 
 // Update the "Current Version" text
@@ -50,9 +59,35 @@ async function loadDeck() {
 }
 
 async function createDeck() {
-    // const transaction = db.transaction("commander");
-    // const index = transaction.objectStore("commander").index("colorIdentityIndex");
-    // const cursor = index.openCursor();
-    // const colorDeck = await db.getFromIndex("commander", "colorIdentityIndex", ["U"]);
+    const colorlessDeck = await db.getAllFromIndex("commander", "colorlessIdentityIndex", []); // this kinda works?
+    console.log("Colorless: " + colorlessDeck.length);
+    let colorDeck = await db.getAllFromIndex("commander", "colorIdentityIndex", "B"); // do this for each requested colour
+    console.log("Black: " + colorTwoDeck.length);
+    let colorTwoDeck = await db.getAllFromIndex("commander", "colorIdentityIndex", "W"); // do this for each requested colour
+    console.log("White: " + colorTwoDeck.length);
+    // why do these searches have so many results? colorless/lands aren't in them but they're only like 500 short
+
+    // for (let i = 0; i < colorDeck.length; i++) {
+    //     if (colorDeck[i].colorIdentity.includes("W") && colorDeck[i].colorIdentity.includes("B")) {
+    //         console.log(colorDeck[i]);
+    //     }
+    // }
+
+    const deck = arrayUnique(colorlessDeck.concat(colorDeck).concat(colorTwoDeck));
+    console.log("Combined: " + deck.length);
+
+    // take the combined results, choose X, and display them as hyperlinks
     alert("Not quite there yet...");
+}
+
+// https://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript-and-de-duplicate-items
+function arrayUnique(array) {
+    var a = array.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i].name === a[j].name)
+                a.splice(j--, 1);
+        }
+    }
+    return a;
 }
